1eeb8813454e7e6d94c57ad8ba5118465b791b30
JustYuvaraj <yuvarajnothing@gmail.com>
2025-03-17 14:54:35 +0530

    fix : CRYPTOGRAPHIC_HASHES.js

--- CRYPTOGRAPHIC_HASHES.js
+++ CRYPTOGRAPHIC_HASHES.js
@@ -1,20 +1,19 @@
-/*### **Steps to Hash a Color**  
+/*### **Steps to Hash **  
 1. Convert string to bytes → `utf8ToBytes(color)`.  
-2. Compute hash → `sha256(bytes)`.  
+2. Compute hash → `sha256(bytes)` its a array so
 3. Convert to hex → `toHex(hash)`.  */
-const {sha256} = require("ethereum-cryptographic/sha256")
-const {toHex , utf8ToBytes} = require("ethereum-cryptography/utils")
+const {sha256} = require('ethereum-cryptographic/sha256')
+const {toHex , utf8ToBytes} = require('ethereum-cryptography/utils')
 let array = ["yuvaraj" , "lakshmi" , "narayanan" , "sasikumar"]
-const bytes = utf8ToBytes(array[-1])
-const hash = sha256(bytes)
-const hex = toHex(hash)
-function findFriend(hex) {
+const toByte = utf8ToBytes("sasikumar")
+const toHash = sha256(toByte)
+function findFriend(toHash) {
     for (let i = 0; i < array.length ; i++){
-        let byte = utf8ToBytes(COLORS[i])
-        let ash = sha256(byte)
-        if(toHex(ash) === hex){
-            return COLORS[i]
+        let byte = utf8ToBytes(array[i])
+        let hash = sha256(byte)
+        if(toHex(hash) === toHex(toHash)){
+            return array[i]
         }
     }
 }
-console.log(findFriend(hex))
+console.log(findFriend(toHash))
